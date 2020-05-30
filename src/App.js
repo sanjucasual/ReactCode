0bb581fe7fd5f5e145f9fa66f8e30d9aa675ec45
sanjeevsharmatech@gmail.com <sanjeevsharmatech@gmail.com>
2020-05-30 12:14:21 +0530

    changes made to use react hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
-class App extends Component {
-  render() {
-    return (
+import person from './Person/Person';
+//ffirst thing create a return fcunstion like person ,cause we are using reacthooks
+
+
+const app=(prop)=>{
+
+  const [personState,personStateset]=useState({
+    person :[{name:"sanjee",age:28},{name:"Aarthi",age:21},{name:"Rajeev",age:25}],
+  otherState:"this wont be changed"
+}
+);
+
+const toggleInput = ()=>{
+
+  // console.log("i have been clicked");
+ personStateset({person :  [
+    {name:"sanjeev",age:28},
+    {name:"Aarthi sanjeev sharma",age:21} ,
+    {name:"Rajeev",age:25}
+  ]
+});
+};
+
+
+  return (
     <div className="App">
+      {/* //dont add paranthesis to function name */}
+      <button onClick={toggleInput}>Change</button>
       <h1>
         Hello world my first component
       </h1>
-<Person name='Sanjeev' age="30"></Person>
-<Person name='Aarthi' age="25">My hobbies are games</Person>
-<Person name='Rajeev' age="29"></Person>
+<Person name={personState.person[0].name} age={personState.person[0].age}></Person>
+<Person name={personState.person[1].name} age={personState.person[1].age}></Person>
+<Person name={personState.person[2].name} age={personState.person[2].age}></Person>
+    <p>{personState.otherState}</p>
     </div>
     );
-  }
-}
 
-export default App;
+
+
+
+} 
+export default app;
+
+
+
+
+
+
+
+
+
+ 
